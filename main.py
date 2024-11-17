dd0d42c9e771884730e4f85958efde174680c99e
Aayush Kumbhare <kumbhareaayush@gmail.com>
2024-11-16 16:17:38 -0800

    Added API request from Google Maps; Added distance calculation; Need to do: Convert reverse_geocode into an address by iteratring through data

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import requests
 from random import randint
-from latlon import LatLon
-from geopy import geopy
+import googlemaps
+from datetime import datetime
 
+gmaps = googlemaps.Client(key='AIzaSyDHNeKahrDnpQJ7Wb-i6tTrSviE6S7nYfA')
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+print(reverse_geocode_result)
+#origin = reverse_geocode_result
+destination = "New York City, NY"
+
+#result = gmaps.distance_matrix(origin, destination, mode="driving")
+
+#distance = result['rows'][0]['elements'][0]['distance']['text']
+#duration = result['rows'][0]['elements'][0]['duration']['text']
+
+#print(f"Distance: {distance}")
+#print(f"Duration: {duration}")
 output = randint(1,100) # will be replaced by the LLM and output from the INIRIX API
 threshold = 30 #subject to change depending on data analysis
 
@@ -14,7 +28,7 @@ def evaluate_congestion (score):
     else:
         return False
 
-print(evaluate_congestion(output))
+#print(evaluate_congestion(output))
 
 
 speed_threshold = 10
